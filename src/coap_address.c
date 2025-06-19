20b99cafbf74a2f963041153eea7889b48560ebb
fj-blanco <fjavib@gmail.com>
2025-06-19 17:08:54 +0200

    Add IN_MULTICAST macro definition for Zephyr

--- src/coap_address.c
+++ src/coap_address.c
@@ -40,6 +40,12 @@
 #define IN_MULTICAST(Address) (0)
 #endif /* RIOT_VERSION */
 
+#ifdef __ZEPHYR__
+#ifndef IN_MULTICAST
+#define IN_MULTICAST(a) ((((long int) (a)) & 0xf0000000) == 0xe0000000)
+#endif
+#endif /* __ZEPHYR__ */
+
 uint16_t
 coap_address_get_port(const coap_address_t *addr) {
   assert(addr != NULL);
